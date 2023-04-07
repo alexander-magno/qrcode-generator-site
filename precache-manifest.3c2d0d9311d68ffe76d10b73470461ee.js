cdaab062a51a5b0dce2fb6b2a321328fc2b3eefe
alexander-magno <118988762+alexander-magno@users.noreply.github.com>
2023-04-07 10:53:02 -0300

    Initial commit

---
+++ precache-manifest.3c2d0d9311d68ffe76d10b73470461ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba7c0accaf56a2479adf",
+    "url": "./static/css/main.ea2dccb5.chunk.css"
+  },
+  {
+    "revision": "ba7c0accaf56a2479adf",
+    "url": "./static/js/main.ba7c0acc.chunk.js"
+  },
+  {
+    "revision": "071f6974c376b039043f",
+    "url": "./static/js/1.071f6974.chunk.js"
+  },
+  {
+    "revision": "f3be609742bb841c4000",
+    "url": "./static/js/2.f3be6097.chunk.js"
+  },
+  {
+    "revision": "3e97e9963f1481e88a21",
+    "url": "./static/js/runtime~main.3e97e996.js"
+  },
+  {
+    "revision": "43a3145e3bd14bcefd95cd40ad2a7764",
+    "url": "./index.html"
+  }
+];
